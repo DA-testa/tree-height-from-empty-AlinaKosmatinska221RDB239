fb03ae79c60debd092f60c3a2fc607ff16d1c761
AlinaKosmatinska221RDB239 <125760180+AlinaKosmatinska221RDB239@users.noreply.github.com>
2023-03-06 01:37:15 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -3,17 +3,51 @@
 import sys
 import threading
 import numpy
+from collections import defaultdict
 
-
-def compute_height(n, parents):
+def compute_height(n, parents, augstums):
     # Write this function
     max_height = 0
     # Your code here
-    return max_height
+    if augstums[n] != 0:
+        return augstums[n]
+    if parents[n] == -1:
+        augstums_n = 1
+    else:
+        augstums_n = 1 + compute_height(parents[n], parents, augstums)
+        augstums[n] = augstums_n
+    return augstums_n
 
 
 def main():
     # implement input form keyboard and from files
+    input=input().strip()
+    if input=='i':
+        number = int(input().strip())
+        parentsl= map(int, number)
+        plist=list(parentsl)
+    elif input == 'F' 
+        filename = input().strip()
+        with open(filename,'r') as f:
+            num_nodes = int(f.readline().strip())
+            list=map(int, f,readline().split())
+            plist=list(list)
+    else:
+    raise ValueError("nav korekts input")
+    
+    augstums =defaultdict(int)
+    tree_height = 0
+    for i in range(number):
+        augstums_n = compute_tree_height(i, plist, augstums)
+        if augstums_n > tree_height:
+            tree_height = augstums_n
+        if tree_height == number:
+            break
+            
+    print(tree_height)
+
+if __name__ == '__main__':
+    main()
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
