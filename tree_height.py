32e3512ff89a1ab60b3a8d2c4c7c1b021616e478
AlinaKosmatinska221RDB239 <125760180+AlinaKosmatinska221RDB239@users.noreply.github.com>
2023-03-06 01:58:39 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,66 +2,65 @@
 
 import sys
 import threading
-import numpy
-from collections import defaultdict
+
 
 def compute_height(n, parents, augstums):
     # Write this function
-    max_height = 0
     # Your code here
-    if augstums[n] != 0:
-        return augstums[n]
-    if parents[n] == -1:
-        augstums_n = 1
-    else:
-        augstums_n = 1 + compute_height(parents[n], parents, augstums)
-        augstums[n] = augstums_n
-    return augstums_n
+    path_lengths = [0] * n
+    visited = [False] * n
+
+    for node in range(n):
+        if not visited[node]:
+            visited[node] = True
+            path_lengths[node] += 1
+            previous = [node]
+            current = parents[node]
+            while current != -1:
+                parent = parents[current]
+                if not visited[current]:
+                    previous.append(current)
+                    for n in previous:
+                        path_lengths[n] += 1
+                    visited[current] = True
+                    current = parent
+                else:
+                    for n in previous:
+                        path_lengths[n] += path_lengths[current]
+                    break
+
+    max_lenght = 0
+    for l in path_lengths:
+        if l > max_lenght:
+            max = l 
+
+    return max
 
 
 def main():
-    # implement input form keyboard and from files
-    input=input().strip()
-    if input=='i':
-        number = int(input().strip())
-        parentsl= map(int, number)
-        plist=list(parentsl)
-    elif input == 'F':
-        filename = input().strip()
-        with open(filename,'r') as f:
-            num_nodes = int(f.readline().strip())
-            list=map(int, f,readline().split())
-            plist=list(list)
+    inp = input().strip()
+    if inp.lower() == "I":
+        n = int(input().strip())
+        input_parents = input().strip().split()
+        parents = [int(x) for x in input_parents]
+        result = compute_height(n, parents)
+        print(result)
+    elif inp.lower() == "F":
+        file = input().strip()
+        if not "a" in file.lower():
+            with open(file, "r") as f:
+                n = int(f.readline().strip())
+                input_parents = f.readline().strip().split()
+                parents = [int(x) for x in input_parents]
+                result = compute_height(n, parents)
+                print(result)
     else:
-    raise ValueError("nav korekts input")
-    
-    augstums =defaultdict(int)
-    tree_height = 0
-    for i in range(number):
-        augstums_n = compute_tree_height(i, plist, augstums)
-        if augstums_n > tree_height:
-            tree_height = augstums_n
-        if tree_height == number:
-            break
-            
-    print(tree_height)
+        print("incorrect input")
 
-if __name__ == '__main__':
-    main()
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+if __name__ == '_main_':
+    sys.setrecursionlimit(10**7)
+    threading.stack_size(2**27)
+    thread = threading.Thread(target=main)
+    thread.start()
 main()
-# print(numpy.array([1,2,3]))
